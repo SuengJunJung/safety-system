8728c23a7c70e0a87bfce9ff32294bf37489fff1
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2022-02-09 17:25:31 -0600

    I am pushing my first commit on this repo!

--- src/main/java/SafetySystem.java
+++ src/main/java/SafetySystem.java
@@ -1,5 +1,6 @@
 public class SafetySystem {
     public static void main(String[] args) {
         System.out.println("Hello world");
+	System.out.println("Showing me I can upload to git");
     }
 }
