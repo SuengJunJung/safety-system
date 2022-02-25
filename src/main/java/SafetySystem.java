27b51cb1311cb2b31963e9a243c01b66a422fb1d
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2022-02-25 16:58:58 -0600

    Add safety system gui

--- src/main/java/SafetySystem.java
+++ src/main/java/SafetySystem.java
@@ -1,6 +1,7 @@
+import javax.swing.*;
+
 public class SafetySystem {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-	System.out.println("Showing me I can upload to git");
+        SafetySystemGui a = new SafetySystemGui();
     }
 }
---
+++ src/main/java/SafetySystemGui.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SafetySystemGui extends JFrame implements ActionListener {
+    public SafetySystemGui() {}
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
