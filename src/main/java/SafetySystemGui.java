337bef9605bb5fdbd710361761cc6e106311bed3
jun <sjung1359@gmail.com>
2022-02-25 18:07:27 -0600

    replace template add layout

--- src/main/java/SafetySystemGui.java
+++ src/main/java/SafetySystemGui.java
@@ -5,36 +5,42 @@ import java.awt.event.ActionListener;
 
 public class SafetySystemGui extends JFrame implements ActionListener
 {
-    private JLabel prompt = new JLabel("Next Grade");
-    private JTextField inField;
-    private JTextArea display;
-    private JButton reset, displayStats;
+    private JLabel emailLabel = new JLabel("Email");
+    private JTextField emailPrompt= new JTextField(10);
+    private JButton send = new JButton("SEND");
+    private JButton inputEmail = new JButton("Input Email");
+    private JPanel emailLayout = new JPanel(new BorderLayout());
+
 
     public SafetySystemGui()
     {
-
+        emailLayout.setMaximumSize(new Dimension(10000, 100000));
+        emailLayout.setMinimumSize(new Dimension(400, 400));
         setLayout(new FlowLayout());  //added
-        inField = new JTextField(4);
-        display = new JTextArea("", 5,20);
-        reset = new JButton("RESET");
-        displayStats = new JButton("STATS");
+        emailLayout.add(inputEmail, BorderLayout.SOUTH );
+        emailLayout.add(emailLabel, BorderLayout.NORTH );
+        emailLayout.add(emailPrompt, BorderLayout.CENTER );
 
-        add(prompt);
-        add(inField);
-        inField.addActionListener( this );
 
-        add(display);
 
-        add(reset);
-        reset.addActionListener( this );
-        add(displayStats);
-        displayStats.addActionListener( this );
-        setSize(600,200);
+        add(emailLayout);
+        emailPrompt.addActionListener( this );
+        inputEmail.addActionListener(this);
+
+
+        add(send);
+        send.addActionListener( this );
+
+        setSize(1920,1080);
+        setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e)
     {
-
+        if (e.getSource() == inputEmail) {
+            System.out.println("ran!");
+            emailPrompt.setText("");
+        }
     }
 }
