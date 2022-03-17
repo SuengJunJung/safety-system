e69328a894864e30b17b1e6c299ab550689eb7bd
Arvinder Dhanoa <ArvinderDhan@gmail.com>
2022-03-17 16:34:25 -0500

    Add variable and template support to GUI (not hooked up yet).

--- src/main/java/SafetySystemGui.java
+++ src/main/java/SafetySystemGui.java
@@ -1,26 +1,30 @@
+import responses.ResponseKind;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class SafetySystemGui extends JFrame implements ActionListener
 {
-    private JLabel emailLabel = new JLabel("Email");
+    private JLabel emailLabel = new JLabel("responses.Email");
     private JTextField emailPrompt= new JTextField(10);
     private JButton send = new JButton("SEND");
-    private JButton inputEmail = new JButton("Input Email");
+    private JButton inputEmail = new JButton("Input responses.Email");
     private JPanel emailLayout = new JPanel(new BorderLayout());
+    private JTextArea contentTemplate = new JTextArea();
+    private JTextArea variables = new JTextArea();
+    private ArrayList<ResponseKind> responses;
 
 
     public SafetySystemGui()
     {
-        emailLayout.setMaximumSize(new Dimension(10000, 100000));
         emailLayout.setMinimumSize(new Dimension(400, 400));
         setLayout(new FlowLayout());  //added
         emailLayout.add(inputEmail, BorderLayout.SOUTH );
         emailLayout.add(emailLabel, BorderLayout.NORTH );
         emailLayout.add(emailPrompt, BorderLayout.CENTER );
-
 
 
         add(emailLayout);
@@ -30,6 +34,13 @@ public class SafetySystemGui extends JFrame implements ActionListener
 
         add(send);
         send.addActionListener( this );
+
+        contentTemplate.setMinimumSize(new Dimension(300, 300));
+        contentTemplate.setPreferredSize(new Dimension(500, 500));
+        add(contentTemplate);
+
+        variables.setPreferredSize(new Dimension(200, 200));
+        add(variables);
 
         setSize(1920,1080);
         setVisible(true);
